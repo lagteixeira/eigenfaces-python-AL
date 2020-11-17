6bfbebaa421de187ef8c7742bf184f6bc7abcc4a
Luís Antônio Granja <lagteixeira2015@gmail.com>
2020-11-17 14:34:33 -0300

    Média das faces

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import glob
 import cv2
 import numpy as np
 import sys
+from skimage import img_as_float, img_as_ubyte
 
 
 class FaceSet(object):
@@ -9,7 +10,8 @@ class FaceSet(object):
         self.directory = directory
         self.load_images()
         self.info()
-        self.flatten()
+        self.recenter()
+        self.get_average()
 
     def load_images(self):
         self.jpegs = glob.glob(self.directory + '/*.jpeg')
@@ -29,9 +31,19 @@ class FaceSet(object):
         self.min_cols = min_cols
         print("\n==> Least common image size:", min_rows, "x", min_cols, "pixels")
         
-    def flatten(self):
-        imgs = np.array([cv2.imread(i, 1).flatten() for i in self.jpegs])
-        self.imgs = imgs
+    def recenter(self):
+        images = self.imgs
+        for (i, image) in enumerate(images):
+            r, c = image.shape[0], image.shape[1]
+            images[i] = image.flatten()[int((r*c - self.min_rows*self.min_cols)/2):int((r*c + self.min_rows*self.min_cols)/2)]
+        self.imgs = images
+    
+    def get_average(self):
+        images = self.imgs
+        for (i, image) in enumerate(self.imgs):
+            images[i] = img_as_float(image)
+        self.average = img_as_ubyte(np.average(images))
+        return self.average
         
     
             
